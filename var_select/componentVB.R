637033eaaec35a3f02a3dd3e6e448678cf591afb
echuu <chuu3ric@gmail.com>
2018-09-15 21:06:54 -0500

    vb alg1 outline

--- var_select/componentVB.R
+++ var_select/componentVB.R
@@ -1,5 +1,36 @@
 
+## componentVB.R
+## first algorithm described in Huang, Wang, Liang paper
+## component-wise variational bayes
 
+n = 40
+p = 8   # dimension of regression coefficients
 
+## true parameters
+beta0 = as.matrix(c(3, 1.5, 0, 0, 2, 0, 0, 0))
+sigma = 3 
+
+# init: (mu_1, ..., mu_p), (sigmasq_1, ..., sigmasq_p), (phi_1, ..., phi_p),
+#       theta_hat, sigmasq_hat
+
+# iteratively update the parameters (1 to p)
+while (!converge) {
+  for (j in 1:p) {
+    
+    # update q_j (fully specified by mu_j, sigmasq_j)
+    
+    # update phi_j
+    
+  } # end for
+
+  # update theta_hat
+  
+  # update sigma_sq
+  
+  # check for convergence
+  
+} # end while
+
+# display parameter estimates
 
 
